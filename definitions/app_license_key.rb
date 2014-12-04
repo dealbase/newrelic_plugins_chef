2ec7fb9fbff8959945b0821aadf19914538db772
emurphy <emmet@dealbase.com>
2014-12-04 15:11:56 -0800

    initialize resource

--- definitions/app_license_key.rb
+++ definitions/app_license_key.rb
@@ -1,10 +1,13 @@
 define :app_license_key, app: nil do
+  return unless app
+  return if node[:newrelic][app[:name]] && node[:newrelic][app[:name]][:license_key]
 
   params[:app][:components].each do |component|
     if component[:collection]
       component[:collection].each do |add_on|
         if add_on[:name] =~ /New Relic/
           license_key = add_on[:config][:vars][:license_key]
+          node[:newrelic][app[:name]] ||= {}
           node[:newrelic][app[:name]][:license_key] = license_key
           verify_license_key license_key
         end
