854bb17dbdb9370479847805917f3af8773b7100
emurphy <emmet@dealbase.com>
2014-12-04 15:22:16 -0800

    no return statement, verbose code

--- definitions/app_license_key.rb
+++ definitions/app_license_key.rb
@@ -1,22 +1,23 @@
 define :app_license_key, app: nil do
   app = params[:app]
-  return unless app
+  if app
 
-  node[:newrelic][app[:name]] ||= {}
-  return if node[:newrelic][app[:name]][:license_key]
+    node[:newrelic][app[:name]] ||= {}
+    unless node[:newrelic][app[:name]][:license_key]
 
-  app[:components].each do |component|
-    if component[:collection]
-      component[:collection].each do |add_on|
-        if add_on[:name] =~ /New Relic/
-          license_key = add_on[:config][:vars][:license_key]
-          node[:newrelic][app[:name]][:license_key] = license_key
-          verify_license_key license_key
+      app[:components].each do |component|
+        if component[:collection]
+          component[:collection].each do |add_on|
+            if add_on[:name] =~ /New Relic/
+              license_key = add_on[:config][:vars][:license_key]
+              node[:newrelic][app[:name]][:license_key] = license_key
+              verify_license_key license_key
+            end
+          end
         end
       end
+
+      node[:newrelic][app[:name]][:license_key] ||= nil
     end
   end
-
-  node[:newrelic][app[:name]][:license_key] ||= nil
-
 end
